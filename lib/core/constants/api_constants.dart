8b848a3bae6fddaf49c6e94a41cdb84ad6815ad0
kmrosiek <kmrosiek@gmail.com>
2026-01-20 15:04:54 +0100

    refactor: Moved API base URL to core constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static const String baseUrl = 'https://november7-730026606190.europe-west1.run.app';
+}
--- lib/infrastructure/datasources/image_remote_datasource.dart
+++ lib/infrastructure/datasources/image_remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../../core/constants/api_constants.dart';
 import '../models/random_image_model.dart';
 
 abstract class ImageRemoteDataSource {
@@ -8,13 +9,12 @@ abstract class ImageRemoteDataSource {
 
 class ImageRemoteDataSourceImpl implements ImageRemoteDataSource {
   final http.Client client;
-  static const String _baseUrl = 'https://november7-730026606190.europe-west1.run.app';
 
   ImageRemoteDataSourceImpl({required this.client});
 
   @override
   Future<RandomImageModel> getRandomImage() async {
-    final response = await client.get(Uri.parse('$_baseUrl/image/'));
+    final response = await client.get(Uri.parse('${ApiConstants.baseUrl}/image/'));
 
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body) as Map<String, dynamic>;
